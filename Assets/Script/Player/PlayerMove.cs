7ff8b4c422e7654ccc4dba6943d49426a865867b
Syuto256 <SBA25J10002@edu.sba.ac.jp>
2025-08-18 17:20:54 +0900

    PlayerMoveの作成をしました

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpSpeed = 10f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private bool isGrounded;
+
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+    }
+
+
     void Update()
     {
-        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        if (Input.GetKeyDown(KeyCode.Space)&&isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed);
+        }
+
+    }
+   
+
+    private bool IsGround()
+    {
+        // 地面との接触判定
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f, groundLayer);
+        return hitColliders.Length > 0;
     }
 }
