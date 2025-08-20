2e3662ecb06f48aa8eeb7c645ce5187dc87ed5b2
Syuto256 <SBA25J10002@edu.sba.ac.jp>
2025-08-20 17:18:26 +0900

    Highスコアが出るようにエラーを直した

--- Assets/Script/GameBase/ScoreManager.cs
+++ Assets/Script/GameBase/ScoreManager.cs
@@ -8,12 +8,17 @@ public class ScoreManager : MonoBehaviour
 
     public int score = 0;
 
+    // UIオブジェクトはシーンが変わるたびに参照を再取得
+    private TextMeshProUGUI gameScoreText;
+    private TextMeshProUGUI highScoreText;
+
     void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded; // シーンロード時のイベントを登録
         }
         else
         {
@@ -21,7 +26,23 @@ public class ScoreManager : MonoBehaviour
         }
     }
 
-    // ゲーム開始時にスコアをリセット
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // シーンが変わるたびにUIの参照を更新する
+        gameScoreText = GameObject.FindWithTag("GameScoreText")?.GetComponent<TextMeshProUGUI>();
+        highScoreText = GameObject.FindWithTag("HighScoreText")?.GetComponent<TextMeshProUGUI>();
+
+        if (scene.name == "Game")
+        {
+            ResetScore();
+            UpdateGameScoreUI();
+        }
+        else if (scene.name == "Title")
+        {
+            DisplayHighScore();
+        }
+    }
+
     public void ResetScore()
     {
         score = 0;
@@ -30,16 +51,32 @@ public class ScoreManager : MonoBehaviour
     public void AddScore(int amount)
     {
         score += amount;
+        UpdateGameScoreUI();
     }
 
-    // ハイスコアを保存
     public void SaveHighScore()
     {
         int savedHighScore = PlayerPrefs.GetInt("HighScore", 0);
         if (score > savedHighScore)
         {
             PlayerPrefs.SetInt("HighScore", score);
-            Debug.Log("ハイスコアを更新しました: " + score);
+        }
+    }
+
+    private void UpdateGameScoreUI()
+    {
+        if (gameScoreText != null)
+        {
+            gameScoreText.text = "Score:" + score.ToString();
+        }
+    }
+
+    private void DisplayHighScore()
+    {
+        int savedHighScore = PlayerPrefs.GetInt("HighScore", 0);
+        if (highScoreText != null)
+        {
+            highScoreText.text = "High Score:" + savedHighScore.ToString();
         }
     }
 }
